b5f1ab5aad5a90e6521d5b3440e828932c9f9217
Isaín Cuadra Rivas <Isan Cuadra Rivas>
2017-04-20 10:49:35 -0500

    Initial commit

---
+++ ios/src/mx/izo/metodos/IOSLauncher.java
@@ -0,0 +1,22 @@
+package mx.izo.metodos;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import mx.izo.metodos.Menu;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Menu(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
