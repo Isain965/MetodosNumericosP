db1bb3a83c8b80a9f681cd683c70a18fe37688b9
Izo965 <isain_5@hotmail.com>
2017-04-20 11:18:36 -0500

    Pantalla

--- core/src/mx/izo/metodos/PrincipalPantalla.java
+++ core/src/mx/izo/metodos/PrincipalPantalla.java
@@ -5,10 +5,8 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -92,6 +90,8 @@ public class PrincipalPantalla implements Screen {
 
         // Entre begin-end dibujamos nuestros objetos en pantalla
         batch.begin();
+
+
         batch.end();
 
     }
