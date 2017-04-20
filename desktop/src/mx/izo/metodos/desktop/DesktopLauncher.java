b961b84933fb8f14baacc2f6560ce0e95b7a54c1
Izo965 <isain_5@hotmail.com>
2017-04-20 11:09:01 -0500

    Pantalla

--- desktop/src/mx/izo/metodos/desktop/DesktopLauncher.java
+++ desktop/src/mx/izo/metodos/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package mx.izo.metodos.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import mx.izo.metodos.Menu;
+
+import mx.izo.metodos.Plataforma;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Menu(), config);
+		new LwjglApplication(new Plataforma(), config);
 	}
 }
